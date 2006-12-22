499da6e8b401135513053edaec26bbdb6402d4b6
mfederico <mfederico@ee1e936f-7723-0410-939f-cd31102c90e6>
2006-12-22 11:57:37 +0000

    util.h: removed the FloorScore function (no more used) in order to keep compatibility with the Moses version of util.h

--- src/util.h
+++ src/util.h
@@ -5,7 +5,6 @@
 #include <fstream>
 #include "gzfilebuf.h"
 
-const float LOWEST_SCORE        = -100.0f;
 
 std::string gettempfolder();
 void createtempfile(std::ofstream  &fileStream, std::string &filePath, std::ios_base::openmode flags);
@@ -27,10 +26,6 @@ public:
 void *MMap(int	fd, int	access, off_t	offset, size_t	len, off_t	*gap);
 int Munmap(void	*p,size_t	len,int	sync);
 
-inline float FloorScore(float logScore)
-{
-        return (std::max)(logScore , LOWEST_SCORE);
-}
 
 #endif
 
