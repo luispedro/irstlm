85ff20f620db3dbb6b5a67cafe7605b1af63ebaa
mfederico <mfederico@ee1e936f-7723-0410-939f-cd31102c90e6>
2007-06-06 09:58:24 +0000

    Added test for build-lm and removed test for build-sublm

--- regression-testing/IrstLMRegressionTesting.pm
+++ regression-testing/IrstLMRegressionTesting.pm
@@ -2,6 +2,9 @@ package IrstLMRegressionTesting;
 
 use strict;
 
+die "Cannot find variable IRSTLM in environment\n"
+if !$ENV{IRSTLM};
+
 # if your tests need a new version of the test data, increment this
 # and make sure that a irstlm-regression-tests-vX.Y is available
 use constant TESTING_DATA_VERSION => '0.1';
@@ -16,7 +19,6 @@ sub find_data_directory
   my $mrtp = "irstlm-reg-test-data-$data_version";
   push @ds, $data_dir if defined $data_dir;
   push @ds, "$test_script_root/$mrtp";
-  push @ds, "/Users/nicolabertoldi/workspace/irstlm/$mrtp";
   push @ds, "/tmp/$mrtp";
   push @ds, "/var/tmp/$mrtp";
   foreach my $d (@ds) {
