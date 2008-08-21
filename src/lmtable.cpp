2f0f100736d2b1685d04a634a4df83cb3bc9e2fe
falavi <falavi@ee1e936f-7723-0410-939f-cd31102c90e6>
2008-08-21 21:16:39 +0000

    lm-compile with lprobx

--- src/lmtable.cpp
+++ src/lmtable.cpp
@@ -579,7 +579,7 @@ void lmtable::loadtxt(istream& inp,const char* header){
             int tmp=maxlev;
             maxlev=Order-1;
             //cerr << ng << "rbow: " << rbow << "prob: " << prob << "low-prob: " << lprob(ng) << "\n"; 
-            prob= log(exp(prob * M_LN10) +  exp((rbow + M_LN10) * M_LN10))/M_LN10;
+            prob= log(exp(prob * M_LN10) +  exp((rbow + lprob(ng)) * M_LN10))/M_LN10;
             //cerr << "new prob: " << prob << "\n"; 
             
             maxlev=tmp;
